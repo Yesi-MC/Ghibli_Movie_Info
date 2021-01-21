3531b5cb976b2846a5bd7a73dd5f93337d38d228
Yesi Meza <yesi.meza10@gmail.com>
2021-01-21 13:52:47 -0700

    create api response

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController 
   def index 
-    
+    conn = Faraday.new("https://ghibliapi.herokuapp.com")
+    response = conn.get("/films")
+    @movies = JSON.parse(response.body, symbolize_names: true )
   end
 end
