26393c8a7411f17878c2efa8476b5dc1171f2033
Yesi Meza <yesi.meza10@gmail.com>
2021-01-21 13:30:46 -0700

    started with test for root path

---
+++ spec/features/user_can_view_all_movies_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe "When I visit the welcome page '/'" do
+    it "can view all Ghibli movies" do 
+      visit root_path 
+      
+      click_button "View all Movies" 
+
+      expect(current_path).to eq(movies_path)
+    end 
+  end 
+end
